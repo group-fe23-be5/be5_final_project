b985d81ab5e707f71ea8ede463d6c109b88b39ae
reonaldi152 <reo@isysedge-soft.com>
2023-06-16 19:45:55 +0700

    cors3

--- src/index.js
+++ src/index.js
@@ -17,10 +17,14 @@ const port = process.env.PORT || 3000;
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://eduart-fe23-be5.netlify.app/',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 app.use('/assets', express.static('uploads'))
 
 // Sinkronisasi model dengan database
