050945fb5ab3ab865183897d43406cfc7f3f7bf1
reonaldi152 <reo@isysedge-soft.com>
2023-06-16 19:54:42 +0700

    cors4

--- src/index.js
+++ src/index.js
@@ -17,10 +17,7 @@ const port = process.env.PORT || 3000;
 
 
 // Middleware
-app.use(cors({
-  origin: 'https://eduart-fe23-be5.netlify.app/',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
