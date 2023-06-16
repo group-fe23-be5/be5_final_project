b576c48898f0840613015607382552afaef09cd9
reonaldi152 <reo@isysedge-soft.com>
2023-06-16 19:41:12 +0700

    cors2

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ const port = process.env.PORT || 3000;
 
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors())
 app.use('/assets', express.static('uploads'))
 
 // Sinkronisasi model dengan database
