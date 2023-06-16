b266dd549ee30a4ad2b3ef1bf4c8ce176b775b5e
reonaldi152 <reo@isysedge-soft.com>
2023-06-17 00:17:03 +0700

    cors7

--- src/index.js
+++ src/index.js
@@ -17,10 +17,7 @@ const port = process.env.PORT || 3000;
 
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
